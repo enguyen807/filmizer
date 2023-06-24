501f4ef584fa04ae8c8d45457422c0c198fcced4
Eric Nguyen <enguyen807@gmail.com>
2023-06-24 03:59:46 +0300

    Import card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import BaseCard from '@/components/BaseCard.vue';
+const people = ['Actor', 'Actress', 'Director'];
 </script>
 
 <template>
-  <main>
-    <TheWelcome />
-  </main>
+    <main class="relative flex flex-col items-center justify-center overflow-hidden pb-6 sm:pb-12">
+        <h1 class="mb-5 text-3xl font-bold underline">Select what to randomized</h1>
+        <div class="grid grid-cols-1 gap-4 md:grid-cols-3">
+            <BaseCard v-for="p in people" :key="p" :person="p"></BaseCard>
+        </div>
+    </main>
 </template>
