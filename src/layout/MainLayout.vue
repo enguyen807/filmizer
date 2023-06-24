703b80df15d95465283d9a13e65faafd991e0578
Eric Nguyen <enguyen807@gmail.com>
2023-06-24 03:59:25 +0300

    Add main layout

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="max-w-fit mx-auto min-h-screen flex flex-col justify-center">
+        <slot></slot>
+        <hr class="w-full"/>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
