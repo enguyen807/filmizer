d70f97eb0e4dc40c68e0195b2804c34455cfcf4e
Eric Nguyen <enguyen807@gmail.com>
2023-06-24 03:58:45 +0300

    Create basecard

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div
+        class="flex w-56 items-center justify-center overflow-hidden rounded bg-stone-800 shadow-lg hover:cursor-pointer hover:bg-emerald-700 md:h-40 md:w-48"
+    >
+        <div class="p-6">
+            <div class="text-xl font-bold">{{ person }}</div>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps<{
+    person: string;
+}>();
+</script>
